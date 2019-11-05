b48a2429966060fb25f4764e05576a9f3b0fc0b9
Nuwaisir <1605091@ugrad.cse.buet.ac.bd>
2019-11-06 00:25:17 +0600

    Comments added at sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,15 +1,22 @@
 //
 // Created by Nuwaisir on 06/11/2019.
 //
+
+/*
+ *
+ * Runtime : O(n log log n) ; [ Roughly ]
+ *
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define MX 10000010
 
 vector<ll> primes;
-bitset<MX> isNotPrime;
+bitset<MX> isNotPrime;                       // Compact STL for Sieve, better than vector<bool> !!
 
-void sieve(ll n) {
+void sieve(ll n) {                           // Generate primes upto n
     ll i, j;
     isNotPrime[1] = true;
     for (i = 4; i <= n; i += 2) {
@@ -20,7 +27,8 @@ void sieve(ll n) {
         if (!isNotPrime[i]) {
             primes.push_back(i);
             if (i * i <= n) {
-                for (j = i * i; j <= n; j += i + i) {
+                for (j = i * i; j <= n; j += i + i) {         // From j = i * i. Because i * 2, i * 3 are already
+                                                              // crossed out by 2, 3 respectively
                     isNotPrime[j] = true;
                 }
             }

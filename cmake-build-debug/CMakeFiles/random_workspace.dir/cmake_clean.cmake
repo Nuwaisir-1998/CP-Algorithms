b044d0661fbd183d3e1aa6067d77c6c2fbe941d5
Nuwaisir <1605091@ugrad.cse.buet.ac.bd>
2019-11-16 15:33:25 +0600

    workspace update maily

---
+++ cmake-build-debug/CMakeFiles/random_workspace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/random_workspace.dir/random_workspace.cpp.o"
+  "random_workspace"
+  "random_workspace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/random_workspace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

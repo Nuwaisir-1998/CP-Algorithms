9c540307f23980d0166c3c231a69db9af4a4a9cc
Nuwaisir-1998 <57160333+Nuwaisir-1998@users.noreply.github.com>
2020-07-19 16:07:53 +0600

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,41 +1,38 @@
-//
-// Created by nuwaisir on 11/26/19.
-//
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef vector<int> vi;
+typedef vector<ll> vi;
 
 class segmentTree{              // Segment tree for range minimum query
     vi st, A;                   // st is the array for binary tree. Index 1 is the root
-    int n;
-    int left(int p){ return 2*p; }
-    int right(int p){ return 2*p + 1; }
+    ll n;
+    ll left(ll p){ return 2*p; }
+    ll right(ll p){ return 2*p + 1; }
 
-    void build(int p, int L, int R){
+    void build(ll p, ll L, ll R){
         if(L == R){
             st[p] = L;
         }
         else{
             build(left(p), L,(L+R)/2);
             build(right(p),(L+R)/2+1, R);
-            int p1 = st[left(p)];
-            int p2 = st[right(p)];
+            ll p1 = st[left(p)];
+            ll p2 = st[right(p)];
             st[p] = (A[p1] <= A[p2]) ? p1 : p2;
         }
     }
 
-    int RMQ(int p, int L, int R, int i, int j){
+    ll RMQ(ll p, ll L, ll R, ll i, ll j){
         if(L >= i && R <= j) return st[p];
         if(R < i || L > j)  return -1;
-        int p1 = RMQ(left(p), L, (L+R)/2, i, j);
-        int p2 = RMQ(right(p), (L+R)/2+1, R, i, j);
+        ll p1 = RMQ(left(p), L, (L+R)/2, i, j);
+        ll p2 = RMQ(right(p), (L+R)/2+1, R, i, j);
         if(p1 == -1) return p2;
         if(p2 == -1) return p1;
         return (A[p1] <= A[p2]) ? p1 : p2;
     }
 
-    void update(int p, int L, int R, int i, int val){
+    void update(ll p, ll L, ll R, ll i, ll val){
         if(L == R && L == i){
             A[L] = val;
             st[p] = L;
@@ -43,8 +40,8 @@ class segmentTree{              // Segment tree for range minimum query
         else if(i <= R && i >= L){
             update(left(p), L, (L+R)/2, i, val);
             update(right(p), (L+R)/2 + 1, R, i, val);
-            int p1 = st[left(p)];
-            int p2 = st[right(p)];
+            ll p1 = st[left(p)];
+            ll p2 = st[right(p)];
             st[p] = (A[p1] <= A[p2]) ? p1 : p2;
         }
     }
@@ -55,10 +52,10 @@ public:
         st.assign(4*n, 0);
         build(1, 0, n-1);
     }
-    int RMQ(int i, int j){
+    ll RMQ(ll i, ll j){     // returns minimum value form i to j inclusive
         return RMQ(1, 0, n-1, i, j);
     }
-    void update(int i, int val){
+    void update(ll i, ll val){
         update(1, 0, n-1, i, val);
     }
 };
